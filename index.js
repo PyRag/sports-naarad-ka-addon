551780bd2a07f780d935342d62bdba80eec8ad71
Mayank Pratap <mayank7860@gmail.com>
2016-06-25 19:03:48 +0530

    Added required javaScript File for Addon

---
+++ index.js
@@ -0,0 +1,33 @@
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
+
+var button = ToggleButton({ //The button shown in firefox toolbar
+  id: "my-button",
+  label: "my button",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onChange: handleChange
+});
+
+var panel = panels.Panel({
+  width:350,
+  height:375,
+  contentURL: self.data.url("../index.html"),
+  onHide: handleHide
+});
+
+function handleChange(state) {
+  if (state.checked) {
+    panel.show({
+      position: button
+    });
+  }
+}
+
+function handleHide() {
+  button.state('window', {checked: false});
+}
